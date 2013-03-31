ea2623f61ed12b682f3250d09872940be2b5d235
Marcin Grzejszczak <marcin@grzejszczak.pl>
2013-03-31 20:39:33 +0200

    [Mappings] Refactoring - moved creation of the GroovyScriptEngine to the constructor"

--- Groovy/Mappings/src/main/java/com/blogspot/toomuchcoding/factory/TransformerFactoryImpl.java
+++ Groovy/Mappings/src/main/java/com/blogspot/toomuchcoding/factory/TransformerFactoryImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 /**
@@ -20,16 +21,21 @@ public class TransformerFactoryImpl implements TransformerFactory<String, String
     private static Logger LOGGER = LoggerFactory.getLogger(TransformerFactoryImpl.class);
 
     private ScriptFactory scriptFactory;
+    private GroovyScriptEngine groovyScriptEngine;
 
     public TransformerFactoryImpl(ScriptFactory scriptFactory) {
         this.scriptFactory = scriptFactory;
+        try {
+            groovyScriptEngine = new GroovyScriptEngine(GROOVY_SCRIPTS_CLASSPATH);
+        } catch (IOException e) {
+            LOGGER.error("Exception occurred while trying to create the Groovy script engine", e);
+        }
     }
 
     @Override
     public Transformer<String, String> createTransformer() {
         Transformer<String, String> transformerFromScript = null;
         try {
-            GroovyScriptEngine groovyScriptEngine = new GroovyScriptEngine(GROOVY_SCRIPTS_CLASSPATH);
             File scriptFile = scriptFactory.createScript();
             URL scriptAsAClasspathResource = Resources.getResource(GROOVY_SCRIPTS_CLASSPATH + scriptFile.getName());
             Class classFromScript = groovyScriptEngine.loadScriptByName(scriptAsAClasspathResource.getFile());
